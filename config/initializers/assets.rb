c847fd5a71a1385d8d4aa3d4b2474c809a52bddc
csxiaodiao <i@xiaodiao.me>
2019-03-06 17:41:49 +0800

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( ckeditor/*)
 Rails.application.config.assets.precompile += %w(
   admin/base.css
-  application.css
   admin/base.js
 )
